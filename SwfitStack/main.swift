df6cf2ecec79c66d19911cd0723a0b2857b2a8c5
soeun <heart_b_e@naver.com>
2020-12-28 21:32:31 +0900

    ✨ Swift Stack 추가

--- SwfitStack/main.swift
+++ SwfitStack/main.swift
@@ -8,4 +8,26 @@
 
 import Foundation
 
+struct Stack<T> {
+    private var stack: [T] = []
+    
+    private var count: Int {
+        return stack.count
+    }
+    
+    private var isEmpty: Bool {
+        return stack.isEmpty
+    }
+    
+    public mutating func push(_ element: T) {
+        stack.append(element)
+    }
+    
+    public mutating func pop() -> T? {
+        return isEmpty ? nil : stack.removeLast()
+    }
+}
 
+var myStack = Stack<Int>()
+myStack.push(10)
+myStack.pop()
